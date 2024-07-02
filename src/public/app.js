fcfe6d56b45e3001f2d37c729cc586180d1408f1
Eduardo Coelho <educs98@outlook.com>
2024-07-02 17:31:55 -0300

    frontend done, bff core done

---
+++ src/public/app.js
@@ -0,0 +1,103 @@
+let favorites = [];
+
+function showSection(section) {
+  document.getElementById("videosSection").style.display =
+    section === "videos" ? "block" : "none";
+  document.getElementById("favoritesSection").style.display =
+    section === "favorites" ? "block" : "none";
+  if (section === "favorites") {
+    renderFavorites();
+  }
+}
+
+function searchVideos() {
+  const searchQuery = document.getElementById("searchInput").value;
+  const url = `http://localhost:3000/videos?q=${searchQuery}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderVideos(data.items));
+}
+
+function formatISODateToReadable(dateISOString) {
+  const date = new Date(dateISOString);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return date.toLocaleDateString("en-US", options);
+}
+
+function renderVideos(videos) {
+  const videosList = document.getElementById("videosList");
+  videosList.innerHTML = "";
+  videos.forEach((video) => {
+    const date = formatISODateToReadable(video.snippet.publishTime);
+    const videoElement = document.createElement("div");
+    videoElement.innerHTML = `
+                <div class="video-content-cover">
+                    <div class="video-content">
+                        <div class="video-box">
+                            <iframe width="360" height="180" src="https://www.youtube.com/embed/${video.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                        <div class="video-details"> 
+                            <div class="detail">
+                                <h3 class="title">${video.snippet.title}</h3>
+                                <div class="channel-name">${video.snippet.channelTitle}</div>
+                                <div class="views-upload">
+                                    <div class="upload">${date}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="favorite-btn-content">
+                            <button class="favorite-btn" data-video-id="${video.id.videoId}" onclick="toggleFavorite('${video.id.videoId}', '${video.snippet.title}')">Favoritar <i class="ri-star-fill"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+    videosList.appendChild(videoElement);
+  });
+}
+
+function toggleFavorite(videoId, title) {
+  const button = document.querySelector(`button[data-video-id="${videoId}"]`);
+  if (button.classList.contains("favorited")) {
+    button.innerHTML = 'Favoritar <i class="ri-star-fill"></i>';
+    button.classList.remove("favorited");
+  } else {
+    button.innerHTML = 'Remover Favorito <i class="ri-star-fill"></i>';
+    button.classList.add("favorited");
+  }
+  const index = favorites.findIndex((f) => f.id === videoId);
+  if (index > -1) {
+    favorites.splice(index, 1);
+  } else {
+    favorites.push({ id: videoId, title });
+  }
+  renderFavorites();
+}
+
+function renderFavorites() {
+  const favoritesList = document.getElementById("favoritesList");
+  favoritesList.innerHTML = "";
+  document.getElementById(
+    "favoritesCounter"
+  ).textContent = `${favorites.length}`;
+  favorites.forEach((favorite) => {
+    const favoriteElement = document.createElement("div");
+    favoriteElement.innerHTML = `
+                <div class="video-content-cover">
+                    <div class="video-content">
+                        <div class="video-box">
+                            <iframe width="360" height="180" src="https://www.youtube.com/embed/${favorite.id}" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                        <div class="video-details"> 
+                            <div class="detail">
+                                <h3 class="title">${favorite.title}</h3>
+                            </div>
+                        </div>
+                        <div class="favorite-btn-content">
+                            <button class="favorited favorite-btn" onclick="toggleFavorite('${favorite.id}', '${favorite.title}')">Remover Favorito ⭐</button>
+                        </div>
+                    </div>
+                </div>
+                `;
+    favoritesList.appendChild(favoriteElement);
+  });
+}
---
+++ src/server/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const session = require('express-session');
+const app = express();
+const PORT = 3000;
+const baseApiUrl = "https://www.googleapis.com/youtube/v3";
+const api_key = process.env.API_KEY;
+
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
+
+app.get('/videos', async (req, res) => {
+    const searchQuery = req.query.q;
+    const url = `${baseApiUrl}/search?part=snippet&q=${searchQuery}&maxResults=21&type=video&key=${api_key}`;
+    try {
+      const response = await axios.get(url);
+      res.json(response.data);
+    } catch (error) {
+      console.error('Error fetching YouTube data:', error);
+      res.status(500).send('Error fetching data');
+    }
+  });
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
